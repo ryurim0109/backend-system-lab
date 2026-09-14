78cd16fadf5c9409b8cb9447fc9a1a0ff90a53ef
ryurim0109 <dbfla5036@naver.com>
2026-09-14 14:56:40 +0900

    chore : swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const appOptions ={cors: true, bodyParser: true, rawBody: true, forceCloseConnections: true};
+  const app = await NestFactory.create(AppModule, appOptions);
+ 
+  app.setGlobalPrefix('api');
+  
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Backend System Lab')
+    .setDescription('Backend System Lab API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
